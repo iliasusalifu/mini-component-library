8aeae5af34465614a4bd047b66279b5a5a9c87d2
Iliasu Salifu <iliasu.salifu@seamless.se>
2024-10-06 22:24:45 +0000

    finished select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,62 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <OriginalSelect value={value} onChange={onChange}>
+        {children}
+      </OriginalSelect>
+      <NewSelect>
+        <SelectedElem>{displayedValue}</SelectedElem>
+        <IconWrapper>
+          <Icon id="chevron-down"></Icon>
+        </IconWrapper>
+      </NewSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: 300px;
+`;
+
+const OriginalSelect = styled.select`
+  opacity: 0;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+`;
+
+const NewSelect = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  display: inline-block;
+  border-radius: 4px;
+  padding: 8px;
+  color: ${COLORS.gray700};
+
+  ${OriginalSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${OriginalSelect}:focus + & {
+    outline: 1px dashed #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const IconWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  top: 6px;
+`;
+const SelectedElem = styled.div`
+  display: inline-block;
+  padding: 12px 16px;
+  position: relative;
+  color: inherit;
+`;
 
 export default Select;
